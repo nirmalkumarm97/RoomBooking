69f8909d66bad12eaedffcddb0b8103916b958d4
NirmalKumar M <nirmal@ideassion.com>
2024-06-11 14:23:35 +0530

    req

---
+++ source/repos/RoomBooking/Models/Request/FoodTransactionRequest.cs
@@ -0,0 +1,17 @@
+﻿using Models.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Request
+{
+    public class FoodTransactionRequest
+    {
+        public int? Id { get; set; }
+        public int FoodItemId { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
